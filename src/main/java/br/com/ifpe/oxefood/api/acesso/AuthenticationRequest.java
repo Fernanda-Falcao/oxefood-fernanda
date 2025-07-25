655b59c0276b93680ef59a34eb9f7e74c7583f60
Fernanda-Falcão <nanda_oliveirafalcao@hotmail.com>
2025-07-25 16:37:21 -0300

    Atualizando o testes

--- src/main/java/br/com/ifpe/oxefood/api/acesso/AuthenticationRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/acesso/AuthenticationRequest.java
@@ -1,6 +1,6 @@
 package br.com.ifpe.oxefood.api.acesso;
 
-import java.io.Serializable;
+//import java.io.Serializable;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
